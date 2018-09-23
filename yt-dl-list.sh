dd7b3557885653ee0b76e8081cee995201b2e62f
jon <jon@HP-Pavilion-15>
2018-09-23 14:36:04 +0200

    Changes all over

--- yt-dl-list.sh
+++ yt-dl-list.sh
@@ -23,17 +23,21 @@ while read URL NAME; do
     #let "$line+=1"
 
     if [ $URL != "" ]; then
-	line=$((line + 1))
-	if [[ $URL =~ $regex ]]; then
-	    echo
-	    echo -e "Line: $line \n------------------------------"
-	    echo -e "URL: $URL"
-	    echo -e "Name: $NAME"
-	    echo
-	    [ "$URL" ] && youtube-dl -o "$NAME.%(episode)s.%(ext)s" "$URL"
-	else
-	    echo -e "$URL \nis not a valid URL"
-	    exit 2
-	fi
+    	if [[ ! $URL =~ ^\# ]] ; then
+			line=$((line + 1))
+			if [[ $URL =~ $regex ]]; then
+			    echo
+			    echo -e "Line: $line \n------------------------------"
+			    echo -e "URL: $URL"
+			    echo -e "Name: $NAME"
+			    echo
+			    [ "$URL" ] && youtube-dl -o "$NAME.%(episode)s.%(ext)s" "$URL"
+			else
+			    echo -e "$URL \nis not a valid URL"
+			    exit 2
+			fi
+		else
+			echo -e "\nSkipping comment line\n"
+        fi	
     fi
 done < $list
